be9a2122597658c8032dba62c4377b59b62e52b2
Adam Daniels <adam@mediadrive.ca>
2025-02-01 20:52:56 -0500

    Command to update Ruby LSP

--- lua/ruby-lsp/init.lua
+++ lua/ruby-lsp/init.lua
@@ -1,6 +1,24 @@
 local ruby_lsp = {}
 local Job = require('plenary.job')
 
+local function rmdir(dir)
+  local handle = vim.loop.fs_scandir(dir)
+  if handle then
+    while true do
+      local name, type = vim.loop.fs_scandir_next(handle)
+      if not name then break end
+
+      local path = dir .. '/' .. name
+      if type == 'directory' then
+        rmdir(path)
+        else
+        vim.loop.fs_unlink(path)
+      end
+    end
+    vim.loop.fs_rmdir(dir)
+  end
+end
+
 local function configure_lspconfig(config)
   local lspconfig = require('lspconfig')
   lspconfig.ruby_lsp.setup(config)
@@ -8,6 +26,30 @@ end
 
 local function is_ruby_lsp_installed()
   return vim.fn.executable('ruby-lsp') == 1
+end
+
+local function update_ruby_lsp(callback)
+  vim.notify('Updating ruby-lsp...')
+
+  Job:new({
+    command = 'gem',
+    args = { 'update', 'ruby-lsp' },
+    on_exit = function(_j, return_val)
+      if return_val == 0 then
+        vim.schedule(function()
+          vim.notify('Update of ruby-lsp complete!')
+
+          if callback then
+            callback()
+          end
+        end)
+      else
+        vim.schedule(function()
+          vim.notify('Update of ruby-lsp failed!')
+        end)
+      end
+    end
+  }):start()
 end
 
 local function create_autocmds(client, buffer)
@@ -35,6 +77,22 @@ local function create_autocmds(client, buffer)
         vim.fn.setqflist(qf_list)
         vim.cmd('copen')
       end, buffer)
+    end,
+    { nargs = '?', complete = function() return { 'all' } end }
+  )
+
+  vim.api.nvim_buf_create_user_command(buffer, 'RubyLspUpdate', function(opts)
+      -- TODO: Does this work on all buffers?
+      -- Stop LSP
+      vim.cmd("LspStop ruby_lsp")
+      -- Remove .ruby-lsp folder if it exists
+      rmdir('.ruby-lsp')
+
+      -- Run gem update ruby-lsp
+      update_ruby_lsp(function()
+        -- Start LSP
+        vim.cmd("LspStart ruby_lsp")
+      end)
     end,
     { nargs = '?', complete = function() return { 'all' } end }
   )
